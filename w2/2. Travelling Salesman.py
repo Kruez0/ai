2d21009bdd0ad58c821077051b0fd43fb928e255
Kelly Louis <kellyangelica88@gmail.com>
2024-03-07 15:01:37 +0800

    aa

---
+++ w2/2. Travelling Salesman.py
@@ -0,0 +1,45 @@
+import random
+cities = [
+    (0,6),(4,1),(1,2),(5,3),
+    (1,1),(1,3),
+    (2,5),(2,3),
+    (3,7),(3,3)
+]
+
+def distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return ((x2-x1)**2+(y2-y1)**2)**0.5
+
+def tsp(cities):
+    start=random.randint(0, len(cities) - 1)
+    path = [start]
+    visited = set([start])
+    total_distance=0
+    while len(visited) < len(cities):
+        city_now=path[-1]
+        shortest_distance=float('inf')
+        shortest_index=None
+        
+    #loop dmn nanti i di plus stu ktika nyampe ke city brikut abis itu klo dh nympe city akhir ituin blik ke awal.
+        #loop buat cri shortest distance stu per stu dicek contoh kalo mulai dri 0,0 trus mo ke 0,1 cek distance
+        for i in range(len(cities)): 
+            if i not in visited:
+                    dist=distance(cities[city_now],cities[i])
+                    if dist<shortest_distance:
+                        shortest_distance=dist
+                        shortest_index=i
+        if shortest_index is not None:
+            path.append(shortest_index)
+            visited.add(shortest_index)
+            total_distance += shortest_distance
+            print(f"From {cities[city_now]} to {cities[shortest_index]}, Distance: {shortest_distance}")
+
+    total_distance += distance(cities[path[-1]], cities[start])
+    path.append(start)
+    return path, total_distance
+
+path_taken, total_distance = tsp(cities)
+print("Path taken:", path_taken)
+print("Total distance:", total_distance)
+print("Path coordinates:", [cities[index] for index in path_taken])
---
+++ w2/Notes.md
@@ -0,0 +1,4 @@
+For the first part of the code is the same as teachers' code. the next part is what i did by myself. I use youtube videos to understand the concept and chatgpt to only help me remember the things that i forgot and all the things i missed. 
+
+To finish this homework, I watch youtube videos about methods to calculate distance from coordinate and also found out about heuristic method.
+so heuristic method is a method that got not the most optimal solution but still produce a good result. In this method, I first make a random start point and then try to find the shortest distance possible to go to another coordinate. after I found the shortest distance, I move on to that new coordinate and find again another shortest distance from that new coordinate. This all went on until all the coordinates are visited and i finally got the shortest path, total distance, and the coordinates from this heuristic method.
